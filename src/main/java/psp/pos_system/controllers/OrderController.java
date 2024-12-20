e1c27529b9db4263f0f26ba873321c89a054d47e
Jokubas Viliusis <viliusisj@gmail.com>
2024-12-20 13:28:07 +0200

    Quick fix

--- src/main/java/psp/pos_system/controllers/OrderController.java
+++ src/main/java/psp/pos_system/controllers/OrderController.java
@@ -2,13 +2,7 @@ package psp.pos_system.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import psp.pos_system.models.DTO.OrderProductsResponse;
 import psp.pos_system.models.Order;
 import psp.pos_system.services.OrderService;
